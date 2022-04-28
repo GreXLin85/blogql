90a1dda7a7eec0d34e79c7330edaa1966884368d
GreXLin85 <38141908+GreXLin85@users.noreply.github.com>
2022-04-28 17:14:56 +0300

    Cors middleware added * For testing API with ApolloGraphQL Studio

--- server.ts
+++ server.ts
@@ -4,8 +4,12 @@ import { ApolloServer } from 'apollo-server-express'
 import typeDefs from './graphql/schemas';
 import resolvers from './graphql/resolvers';
 import context from './graphql/context'
+import cors from "cors";
 
 const app = express();
+
+// NOTE - This is a temporary solution for development to allow all cross-origin requests
+app.use(cors());
 
 const apolloServer = new ApolloServer({
   typeDefs,
